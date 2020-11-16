a9269d80037c34c0ef3d3cc26efd58d10c422836
ScribbleTons <ugwuokeemmanuel125@gmail.com>
2020-11-16 05:19:22 -0800

    inital commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Checkout Form with Stripe
+This small demo shows how to accept payments with stripe with Vue.js using an Azure Serverless Function.
---
+++ stripe-handler/index.js
@@ -0,0 +1,48 @@
+const STRIPE_KEY = 'sk_test_51HnQGREdOXN3xAkoxRarjsnsKfWBCJ19vC2IRsBgOa2ZRa0QOBoM9XWTQ2Weqn6mbpnFYtypRt6kVaRqq4jO46m300L5pv67h2';
+const configureStripe = require('stripe');
+const stripe = configureStripe(STRIPE_KEY);
+
+module.exports = async function(context, req) {
+    context.log('Getting down');
+    if(
+        req.body &&
+        req.body.stripeEmail &&
+        req.body.stripeToken &&
+        req.body.stripeAmt
+    ) {
+        stripe.customers
+            .create({
+                email: req.body.stripeEmail,
+                source: req.body.stripeToken
+            })
+            .then(customer => {
+                context.log('starting the stripe charges');
+                stripe.charges.create({
+                    amount: req.body.stripeAmt,
+                    description: 'sample charge',
+                    currency: 'usd',
+                    customer: customer.id
+                });
+            }).then(
+                charge => {
+                    context.log('finish the stripe charges');
+                    context.res = {
+                        //status:200
+                        body: 'This has been completed'
+                    };
+                    context.done();
+                })
+            .catch(err => {
+                context.log(err);
+                context.done();
+            });
+    } else {
+        context.log(req.body);
+        contex.res = {
+            status: 400,
+            body: 'We\'re missing something'
+        };
+        context.done();
+    }
+
+}
